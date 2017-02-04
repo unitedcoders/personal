ed7af710d7d716c112551b06d7d2c0ad83826a70
bektur <Muratov225@gmail.com>
2017-02-05 00:11:47 +0600

    create admin

--- admin/admin.php
+++ admin/admin.php
@@ -1,7 +1,13 @@
 <?php
 /**
  * Created by PhpStorm.
+<<<<<<< HEAD
+ * User: bektur
+ * Date: 1/29/17
+ * Time: 11:49 PM
+=======
  * User: Nurbolot
  * Date: 29.01.2017
  * Time: 20:53
+>>>>>>> origin/master
  */
